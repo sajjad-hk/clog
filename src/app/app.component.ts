1fe214c00f70bd91be95a4dc7f32273cdedf5cb2
Sajjad Hassanpour Kasebnia <sajjad.hassanpour@lingarogroup.com>
2018-10-16 17:37:47 +0100

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'clg-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'climbing-log';
+}
