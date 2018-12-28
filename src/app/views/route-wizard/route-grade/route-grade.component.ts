4903aed45304cbc6cffe9f72d3c8b25b78fbd441
sajjadhassanpour <sajjad.hassanpour@lingarogroup.com>
2018-12-28 14:21:22 +0100

    grade typing and knob dbclick

--- src/app/views/route-wizard/route-grade/route-grade.component.ts
+++ src/app/views/route-wizard/route-grade/route-grade.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { ClimbingGrade, GRADE_BASE, IGrade } from 'src/app/models/route';
 
 @Component({
   selector: 'clg-route-grade',
@@ -7,95 +8,37 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 })
 export class RouteGradeComponent implements OnInit {
   
-  FR_SET = [
-    {key: 0, value:'4a'},
-    {key: 1, value:'4a+'},
-    {key: 2, value:'4b'},
-    {key: 3, value:'4b+'},
-    {key: 4, value:'4c'},
-    {key: 5, value:'4c+'},
-    {key: 6, value:'5a'},
-    {key: 7, value:'5a+'},
-    {key: 8, value:'5b'},
-    {key: 9, value:'5b+'},
-    {key: 10, value:'5c'},
-    {key: 11, value:'5c+'},
-    {key: 12, value:'6a'},
-    {key: 13, value:'6a+'},
-    {key: 14, value:'6b'},
-    {key: 15, value:'6b+'},
-    {key: 16, value:'6c'},
-    {key: 17, value:'6c+'},
-    {key: 18, value:'7a'},
-    {key: 19, value:'7a+'},
-    {key: 20, value:'7b'},
-    {key: 21, value:'7b+'},
-    {key: 22, value:'7c'},
-    {key: 23, value:'7c+'},
-    {key: 24, value:'8a'},
-    {key: 25, value:'8a+'},
-    {key: 26, value:'8b'},
-    {key: 27, value:'8b+'},
-    {key: 28, value:'8c'},
-    {key: 29, value:'8c+'},
-    {key: 30, value:'9a'},]
-
-  PL_SET = [
-    {key: 0,value:'IV+'},
-    {key: 1,value:'V-'},
-    {key: 2,value:'V'},
-    {key: 3,value:'V+'},
-    {key: 4,value:'VI-'},
-    {key: 5,value:'VI'},
-    {key: 6,value:'VI+'},
-    {key: 7,value:'VI.1'},
-    {key: 8,value:'VI.1+'},
-    {key: 9,value:'VI.2'},
-    {key: 10,value:'VI.2+'},
-    {key: 11,value:'VI.3'},
-    {key: 12,value:'VI.3+'},
-    {key: 13,value:'VI.4'},
-    {key: 14,value:'VI.4+'},
-    {key: 15,value:'VI.5'},
-    {key: 16,value:'VI.5+'},
-    {key: 17,value:'VI.6'},
-    {key: 18,value:'VI.6+'},
-    {key: 19,value:'VI.7'},
-    {key: 20,value:'VI.7+'},
-    {key: 21,value:'VI.8'},]
-
-    PL_FIRST={key: 2, value: 'V'}
-    FR_FIRST = {key: 6, value: '5a'}
-
-    selectedGrade = this.FR_FIRST
-
     isFR = true
     isPL = false
-
     display = false;
     gradeType: string;
+    grade: ClimbingGrade = { 
+      gradeType: 'FRENCH', 
+      gradeValue: GRADE_BASE.FR_FIRST as IGrade
+    }
 
   @Output() finished: EventEmitter<any> = new EventEmitter();
-  @Output() back: EventEmitter<any> = new EventEmitter();
-  @Output() close: EventEmitter<any> = new EventEmitter();
+  @Output() back: EventEmitter<void> = new EventEmitter();
+  @Output() close: EventEmitter<void> = new EventEmitter();
   constructor() { }
 
   ngOnInit() { }
 
-  onGradeTypeChange(event) {
+  onGradeTypeChange(event: any) {
     console.log({event})
     switch(event.value) {
       case 'FRENCH':
         this.isFR = true
         this.isPL = false
-        this.selectedGrade = this.FR_FIRST
+        this.grade.gradeValue = GRADE_BASE.FR_FIRST
         break
       case 'POLISH':
         this.isFR = false
         this.isPL = true
-        this.selectedGrade = this.PL_FIRST
+        this.grade.gradeValue = GRADE_BASE.PL_FIRST
         break
     }
+    this.grade.gradeType = event.value
   }
 
   show() {
@@ -104,9 +47,10 @@ export class RouteGradeComponent implements OnInit {
   hide() {
     this.display = false;
   }
-  onNext() {
+  onNext(event: any) {
+    console.log({event})
     this.display = false;
-    this.finished.emit({gradeType: this.gradeType, grade: this.selectedGrade.value});
+    this.finished.emit(this.grade);
   }
   onBack() {
     this.display = false;
@@ -121,6 +65,6 @@ export class RouteGradeComponent implements OnInit {
   }
 
   onValueChange() {
-    console.log('%c INFO => Input value changed to:', 'color: green; font-weight: bold;', this.selectedGrade)
+    console.log('%c INFO => Input value changed to:', 'color: green; font-weight: bold;', this.grade)
   }
 }
